bcfacea68c8717b0a199b694b95bbb40a31aea01
rrao24 <rao.ram5718@gmail.com>
2025-05-17 00:42:32 -0400

    Add Cloudfront Terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,81 @@ resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   bucket     = aws_s3_bucket.destination_bucket.id
   acl        = "private"
 }
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+    name                              = "OAC"
+    description                       = "OAC Controls"
+    origin_access_control_origin_type = "s3"
+    signing_behavior                  = "always"
+    signing_protocol                  = "sigv4"
+  }
+  
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled = true
+  
+  origin {
+    domain_name              = aws_s3_bucket.destination_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = "origin-bucket-${aws_s3_bucket.destination_bucket.id}"
+  }
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    min_ttl                = "0"
+    default_ttl            = "500"
+    max_ttl                = "1000"
+    target_origin_id       = "origin-bucket-${aws_s3_bucket.destination_bucket.id}"
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 500
+    error_code            = 404
+    response_code         = "200"
+    response_page_path    = "/404.html"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+    bucket = aws_s3_bucket.destination_bucket.id
+    policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowCloudFrontReadFromS3",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : "s3:GetObject",
+        "Resource" : "${aws_s3_bucket.destination_bucket.arn}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : "${aws_cloudfront_distribution.cdn.arn}"
+          }
+        }
+      }
+    ]
+  })
+}
